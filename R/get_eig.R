7b1b46e59e9d8c5aef6a3307fcd573e845a917bb
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-01-14 22:59:04 +0700

    Init package

---
+++ R/get_eig.R
@@ -0,0 +1,18 @@
+get_eig <- function(.res) {
+  if (missing(.res)) {
+    stop("Data is not supplied", call. = FALSE)
+  } else if (!any(class(.res) %in% c("PCA", "CA", "MFA", "MCA"))) {
+    stop("Data is not one of PCA, CA, MFA, MCA class", call. = FALSE)
+  }
+  .res %>%
+    extract2("eig") %>%
+    as_tibble(rownames = "dim") %>%
+    rename(
+      "var_percent" = `percentage of variance`,
+      "var_cummulative" = `cumulative percentage of variance`
+    ) %>%
+    mutate(dim = str_replace_all(dim, "comp", "Dim"))
+}
+
+
+
