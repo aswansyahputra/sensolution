4c7420a857449c5a60a1c7a069a199f54b127fba
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-01-16 22:51:10 +0700

    update roxygen

--- R/do_pca.R
+++ R/do_pca.R
@@ -1,18 +1,17 @@
-#' @import dplyr magrittr FactoMineR
-NULL
-
 #' Perform PCA on dataframe
 #'
 #' @param .data a dataframe or tibble.
 #' @param product name of column containing product information.
 #' @param liking name of column containing hedonic liking (default to NULL).
 #' @param scale whether to scale data to unit of variance (default to TRUE)
+#'
+#' @import dplyr magrittr FactoMineR
 #' @return Returns a list containing information of PCA results.
+#' @export
+#'
 #' @examples
 #' sensochoc_means %>% do_pca(product = "Product")
 #' do_pca(sensochoc_means, product = "Product")
-#'
-
 do_pca <- function(.data, product, liking = NULL, scale = TRUE) {
   if (missing(.data)) {
     stop("Data is not supplied", call. = FALSE)
